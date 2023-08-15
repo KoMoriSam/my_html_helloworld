6b51c6b246e680280a66d0d5b0e644e58f8868b3
KoMoriSam <1760665075@qq.com>
2023-08-15 21:57:12 +0800

    🔧fixed dark-mode toggle 23.08.09

--- js/mode.js
+++ js/mode.js
@@ -5,19 +5,18 @@ const systemTheme = window.matchMedia('(prefers-color-scheme: dark)');
 const root = document.documentElement;
 
 // 获取切换按钮
-const darkToggle = document.getElementById('dark-toggle');
-const lightToggle = document.getElementById('light-toggle');
+const modeToggle = document.getElementById('mode-toggle');
 
 // 定义一个函数，根据系统亮暗模式设置网页亮暗模式
 function setThemeBySystem() {
     if (systemTheme.matches) {
         // 如果系统是暗模式，设置网页为暗模式
         root.setAttribute('data-theme', 'dark');
-        darkToggle.classList.toggle('header-active');
+        modeToggle.classList.add('ri-moon-line');
     } else {
         // 如果系统是亮模式，设置网页为亮模式
         root.removeAttribute('data-theme');
-        lightToggle.classList.toggle('header-active');
+        modeToggle.classList.add('ri-sun-line');
     }
 }
 
@@ -26,13 +25,13 @@ function toggleThemeByUser() {
     if (root.hasAttribute('data-theme')) {
         // 如果网页是暗模式，切换为亮模式
         root.removeAttribute('data-theme');
-        lightToggle.classList.toggle('header-active');
-        darkToggle.classList.toggle('header-active');        
+        modeToggle.classList.toggle('ri-moon-line');
+        modeToggle.classList.toggle('ri-sun-line');
     } else {
         // 如果网页是亮模式，切换为暗模式
         root.setAttribute('data-theme', 'dark');
-        darkToggle.classList.toggle('header-active');
-        lightToggle.classList.toggle('header-active');
+        modeToggle.classList.toggle('ri-sun-line');
+        modeToggle.classList.toggle('ri-moon-line');
     }
 }
 
@@ -43,5 +42,4 @@ window.addEventListener('load', setThemeBySystem);
 systemTheme.addEventListener('change', setThemeBySystem);
 
 // 当用户点击切换按钮时，根据用户点击切换网页亮暗模式
-darkToggle.addEventListener('click', toggleThemeByUser);
-lightToggle.addEventListener('click', toggleThemeByUser);
+modeToggle.addEventListener('click', toggleThemeByUser);
